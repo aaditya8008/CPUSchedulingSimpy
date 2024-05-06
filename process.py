696d0f2b6b5bc90f0fd0b06b4b7e8747b7c0b4bf
aaditya8008 <114769448+aaditya8008@users.noreply.github.com>
2024-05-06 18:18:43 +0530

    process added

---
+++ process.py
@@ -0,0 +1,10 @@
+class Process:
+    def __init__(self,pid,at,bt):
+        self.pid=pid
+        self.at=at
+        self.bt=bt
+        self.wt=0
+        self.tat=0
+        self.timeQuantum=0
+        self.priority=0
+        self.queue="None"
