3cda2042993e4a33070e20fdf3a27fd94ab95c20
tanviutreja <114743226+tanviutreja@users.noreply.github.com>
2024-05-08 10:45:17 +0530

    started main

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-
+import simpy
+import random
+from process import Process
+from fcfs import FCFS
+from sjf import sjf_nonpreemtive
+from roundRobin import RoundRobin
+def generate_processes(num_processes):
+    processes=[]
+    
 
