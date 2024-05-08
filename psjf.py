2904d35407ca02cd9fa8c2a5fe875ca3db40378a
tanviutreja <114743226+tanviutreja@users.noreply.github.com>
2024-05-08 10:10:32 +0530

    added preemtive sjf

---
+++ psjf.py
@@ -0,0 +1,44 @@
+import simpy
+from process import Process
+class sjf_preemptive:
+   def __init__(self,env):
+      self.env=env
+      self.processlist[Process]=[]
+      tq=4
+      
+      
+   def schedule(self,processes:Process):
+      ready_queue=[]
+      self.processlist.append(processes)
+      current_time=self.env.now
+      self.processlist.sort(key=lambda x:x.at)
+      while self.processlist or ready_queue:
+         if not ready_queue:
+            self.current_time=self.processlist[0].at
+
+         while self.processlist and self.processlist.at<=current_time:
+            ready_queue.append(self.processlist.pop(0))
+         ready_queue.sort(key=lambda x:x.bt)
+         if ready_queue:
+            current_process=ready_queue.pop(0)
+            yield self.env.timeout(1)
+            self.current_time+=1
+
+            while self.processlist and self.processlist[0].at<=self.current_time:
+               new_process=self.process.pop(0)
+               if new_process.bt<current_process.bt:
+                  ready_queue.append(current_process)
+                  current_process=new_process
+               else:
+                  ready_queue.append(new_process)
+            if current_process.bt>0:
+               current_process.bt-=1
+               ready_queue.apppend(current_process)
+
+
+            else:
+               current_process.tat=self.current_time-current_process.at
+               current_process.wt = current_process.tat - current_process.original_bt
+               print(f"Process {current_process.id} completed at time {self.current_time}")
+      for p in self.processlist:
+            p.tat = p.original_bt + p.wt
