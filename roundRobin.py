ffc9b10eaa8e31f155e86047ca9b65fdccc69d37
aaditya8008 <114769448+aaditya8008@users.noreply.github.com>
2024-05-08 10:03:59 +0530

    Completed value added

--- roundRobin.py
+++ roundRobin.py
@@ -1,21 +1,21 @@
 from process import Process
 import simpy
-class FCFS:
+class RoundRobin:
  
  def  __init__(self):  
     self.processList[Process]=[]
-    self.tq=16
+    self.tq=8
 
  
  def schedule(self,env,processes:Process):
    
    self.processList.append(processes)
-   self.processList.sort(key=lambda x:x.at)
+  
 
    current_time=env.now
 
    for p in self.processList:
-    if(p.completed!=True):
+     
      if current_time<p.at:
        current_time=p.at
 
@@ -23,19 +23,12 @@ class FCFS:
        current_time+=p.bt
        p.tat=current_time=p.ar
        p.wt=p.tat-p.bt
-       p.completed=True
      yield env.timeout(p.br)
 
    return self.processList
  
-
+ 
  def state(self):
     print("Pid\tBt\tAt\tTat\tWt\tCompleted")
     for p in self.processes:
             print(f"{p.pid}\t{p.bt}\t{p.at}\t{p.tat}\t{p.wt}\t{p.complete}")
-     
-
-       
-    
-
- 
