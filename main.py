85c7ac48949ce305ac6cffe509ea7d23b326c865
aaditya8008 <114769448+aaditya8008@users.noreply.github.com>
2024-05-09 19:36:36 +0530

    Main started

--- main.py
+++ main.py
@@ -4,7 +4,33 @@ from process import Process
 from fcfs import FCFS
 from sjf import sjf_nonpreemtive
 from roundRobin import RoundRobin
+pid =0
 def generate_processes(num_processes):
     processes=[]
+
+    for i in range(num_processes):
+       at=random.randint(0,10)     #Random Arrival Time
+       bt=random.randint(1,20)     #Random Burstt  Time
+       processes.append(Process(pid=pid,at=at,bt=bt))
+
+def main():
+    num_processes=random.randint(1,10)
+    psjfList=generate_processes(num_processes=num_processes)
+
+    num_processes=random.randint(1,10)
+    rrList=generate_processes(num_processes=num_processes)
+
+    num_processes=random.randint(1,10)
+    sjfList=generate_processes(num_processes=num_processes)
+
+    num_processes=random.randint(1,10)
+    fcfsList=generate_processes(num_processes=num_processes)
+
+    env=simpy.Environment()
+    fcfs=FCFS()
+    roundRobin=RoundRobin()
+
+    
+
     
 
